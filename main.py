3aae46b15600cb45dce37a3e9dc681b472def3ff
Emmanuel Diehl <emmanuel.diehl@ifsr.de>
2025-06-03 11:11:46 +0200

    dynamically populate frontend with fake results from dict

--- main.py
+++ main.py
@@ -19,5 +19,11 @@ def run_textual_app_from_path(file_path: str, class_name: str = "App"):
     app = app_class()
     app.run()
 
+def create_test_results():
+    result_1 = {"doc_id": 123414, "init_ranking": 98, "category": "film", "cluster": 9, "text": "CSV files are the Comma Separated Files. It allows users to load tabular data into a DataFrame, which is a powerful structure for data manipulation and analysis. To access data from the CSV file, we require a function read_csv() from Pandas that retrieves data in the form of the data frame. Here’s a quick example to get you started. "}
+    return [result_1]
+
 if __name__ == "__main__":
     run_textual_app_from_path(frontend_path, class_name="SearchEngineFrontend")
+
+
--- src/frontend/frontend.py
+++ src/frontend/frontend.py
@@ -1,3 +1,4 @@
+from typing import Dict, List
 import pandas as pd
 
 from textual.widget import Widget
@@ -8,6 +9,12 @@ from textual.reactive import reactive
 from rich.text import Text
 from textual.message import Message
 
+class SearchTriggered(Message):
+    def __init__(self, sender: Widget, query: str) -> None:
+        super().__init__()
+        self.sender = sender
+        self.query = query
+
 class SearchEngineFrontend(App):
     CSS_PATH = "style.css"
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
@@ -17,52 +24,47 @@ class SearchEngineFrontend(App):
         yield Header()
         yield Footer()
         yield SearchBar()
-        yield VerticalScroll(
-            ResultField(
-                ranking=1,
-                cluster="A",
-                doc_id="doc_123",
-                text="This is a long document content. It may span multiple lines.\nSecond line of content.\nThird line.\nFourth line.\nFifth line."
-            ),
-            ResultField(
-                ranking=2,
-                cluster="B",
-                doc_id="doc_125",
-                text="This is another very long document content. It may span multiple lines.\nSecond line of content.\nThird line.\nFourth line.\nFifth line."
-            ),
-            ResultField(
-                ranking=2,
-                cluster="B",
-                doc_id="doc_125",
-                text="This is yet another very long document content. Maybe talking about Jaguars. It may span multiple lines.\nSecond line of content.\nThird line.\nFourth line.\nFifth line."
-            ),
-            id="results_container"
-        )
+        yield VerticalScroll(id="results_container")
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
         self.theme = (
             "textual-dark" if self.theme == "textual-light" else "textual-light")
 
-    def populate_results_from_df(self, df: pd.DataFrame) -> None:
+    def populate_results_from_dict(self, result_list: List[Dict]) -> None:
         results_container: Widget = self.query_one("#results_container")
-        results_container.clear()
 
-        top_rows = df.head(10)
+        top_ten = result_list[:10]
 
-        for _, row in top_rows.iterrows():
+        for entry in top_ten:
             rf = ResultField(
-                ranking=row["Original ranking"],
-                cluster=str(row["Cluster ID"]),
-                doc_id=str(row["Document ID"]),
-                text=row["Text"]
+                ranking=entry["init_ranking"],
+                cluster=str(entry["cluster"]),
+                doc_id=str(entry["doc_id"]),
+                text=entry["text"]
             )
             results_container.mount(rf)
+    
+
+    def generate_fake_results(self) -> List[Dict]:
+        return [
+            {"doc_id": 182348321, "init_ranking": 55, "category": "Ohrenschmalzentferner", "cluster": 6, "text": "Der Ohrenschmalzentferner schläft nie"},
+            {"doc_id": 182348322, "init_ranking": 48, "category": "Kaffeemaschine", "cluster": 2, "text": "Die Kaffeemaschine ist kaputt"}
+            ]
+
+    async def on_search_triggered(self, message: SearchTriggered) -> None:
+        fake_results = self.generate_fake_results()
+        self.populate_results_from_dict(fake_results)
 
 class SearchBar(HorizontalGroup):
     def compose(self) -> ComposeResult:
         yield Input(placeholder="Enter search term...", id="search_input", classes="search_input")
         yield Button("Search 🔎", id="start", variant="success")
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "start":
+            query_input = self.query_one("#search_input", Input)
+            self.post_message(SearchTriggered(self, query_input.value))
 
 class ResultField(Vertical):
     def __init__(self, ranking: int, cluster: str, doc_id: str, text: str) -> None:
@@ -79,4 +81,4 @@ class ResultField(Vertical):
                 Static(Text.from_markup(self.text), classes="result-body"),
             ),
             classes="result-frame"
-        )
+                        )
