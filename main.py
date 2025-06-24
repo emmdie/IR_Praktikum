1d7a62012aea3dea5f3f0c90a007eb3e7fc61b92
Emmanuel Diehl <emmanuel.diehl@ifsr.de>
2025-06-24 02:12:36 +0200

    rework frontend to use module structure

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ import importlib.util
 
 # Some useful paths
 script_path = Path(__file__).resolve()
-script_dir = script_path.parent
-frontend_path = script_dir / "src" / "frontend" / "frontend.py"
-    
+repo_root = script_path.parent
+src_path = repo_root / "src"
+sys.path.insert(0, str(src_path))  # Arkane Magie um src/frontend als package einzubinden
+
+from frontend.frontend import SearchEngineFrontend
+
 def run_textual_app_from_path(file_path: str, class_name: str = "App"):
     spec = importlib.util.spec_from_file_location("dynamic_textual_app", file_path)
     module = importlib.util.module_from_spec(spec)
@@ -20,4 +23,5 @@ def run_textual_app_from_path(file_path: str, class_name: str = "App"):
     app.run()
 
 if __name__ == "__main__":
-    run_textual_app_from_path(frontend_path, class_name="SearchEngineFrontend")
+    app = SearchEngineFrontend()
+    app.run()
