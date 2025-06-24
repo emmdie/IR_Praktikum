b8094ca8c370a611529242935e05c26645e19e23
Emmanuel Diehl <emmanuel.diehl@ifsr.de>
2025-06-24 02:15:55 +0200

    remove unnecessary funtion from main

--- main.py
+++ main.py
@@ -11,17 +11,6 @@ sys.path.insert(0, str(src_path))  # Arkane Magie um src/frontend als package ei
 
 from frontend.frontend import SearchEngineFrontend
 
-def run_textual_app_from_path(file_path: str, class_name: str = "App"):
-    spec = importlib.util.spec_from_file_location("dynamic_textual_app", file_path)
-    module = importlib.util.module_from_spec(spec)
-    sys.modules["dynamic_textual_app"] = module
-    spec.loader.exec_module(module)
-    app_class = getattr(module, class_name)
-    if not issubclass(app_class, App):
-        raise TypeError(f"{class_name} is not a subclass of textual.app.App")
-    app = app_class()
-    app.run()
-
 if __name__ == "__main__":
     app = SearchEngineFrontend()
     app.run()
