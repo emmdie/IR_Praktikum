140282aa04fa0ab845fdacbcce75c33e183b0d89
manuelberger981 <manuelberger335@gmail.com>
2025-06-29 18:00:00 +0200

    Update embedding

--- src/embedding/SBERT_document_embedding.py
+++ src/embedding/SBERT_document_embedding.py
@@ -2,23 +2,19 @@ import pandas as pd
 import torch
 from sentence_transformers import SentenceTransformer
 
-print("Hello World!")
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model = SentenceTransformer("all-mpnet-base-v2", device=device)
-print(model)
-df = pd.read_pickle("data/wikipedia/split-data-no-disambiguation/wikipedia-text-data-no-disambiguation_1.pkl.gzip", compression="gzip")
-print(df)
+df = pd.read_pickle("data/wikipedia/split-data-no-disambiguation/wikipedia-text-data-no-disambiguation_0.pkl.gzip", compression="gzip")
+
 
 documents = df.iloc[:, 2].tolist()
-print(documents[:5])
-print("Start embedding documents")
 embeddings = model.encode(documents)
-print("Finished embedding documents")
 ids = df["d_id"].tolist()
-print(ids[:5])
-new_df = pd.DataFrame({'embedding': list(embeddings)})
-print(new_df)
-new_df.insert(0, 'd_id', ids)
-#df = pd.DataFrame({'d_id': ids, 'embedding': embeddings})
-print(new_df)
-new_df.to_pickle("embeddings_1.pkl.gzip", compression="gzip")
+labels = df["label"].tolist()
+new_df = pd.DataFrame({
+    'd_id': ids,
+    'label': labels,
+    'embedding': list(embeddings)
+    })
+new_df.set_index('d_id', inplace=True)
+new_df.to_pickle("embeddings_0.pkl.gzip", compression="gzip")
